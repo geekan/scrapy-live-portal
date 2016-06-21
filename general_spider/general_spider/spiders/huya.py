0634003f1f9fd97bca0b384e33c443e293c4b234
geekan <geekan@foxmail.com>
2016-06-22 00:20:29 +0800

    huya: update

--- general_spider/general_spider/spiders/huya.py
+++ general_spider/general_spider/spiders/huya.py
@@ -1,24 +1,26 @@
+#coding: utf-8
 
 from BasicSpiderConfig import ExRule
 from collections import OrderedDict
 from misc.common import *
 from misc.log import *
+import re
 
 class Config:
 
     list_css_rules = { 
-        '.video-list-item.video-no-tag': {
-            'room_name': '.video-title::text',
-            'anchor': '.video-nickname::text',
-            'audience_count': '.video-number::text',
-            'tag': '.video-cate::text',
-            'url': 'a::attr(href)',
+        '.video-list .video-list-item': {
+            'room_name': '#js-video-all .clickstat::text',
+            'anchor': '.nick::text',
+            'audience_count': '.js-num::text',
+            'tag': '.fr a::text',
+            'url': 'a.video-info::attr(href)',
             'platform': ['huyatv'],
             'platform_prefix_url': ['http://www.huya.com/'],
         }   
     }
 
-    ex_rule = ExRule('http://www.huya.com/all$', list_css_rules=list_css_rules)
+    ex_rule = ExRule('http://www.huya.com/l$', list_css_rules=list_css_rules)
 
     name='huyatv'
     allowed_domains=['huya.com']
@@ -27,15 +29,44 @@ class Config:
     follow = False
 
     @staticmethod
+    def deal_human_readable_numbers(hrn=''):
+        numbers = re.findall(r'\d+\.?\d*', hrn)
+        if len(numbers) == 0:
+            return 0
+        elif len(numbers) > 1:
+            warn('suspect numbers:'+str(numbers))
+
+        number = float(numbers[0])
+
+        # FIXME magic logic. Consider to add log here.
+        if hrn.endswith(u'万'):
+            number = (number * 10000.0)
+        return str(int(number))
+
+    @staticmethod
+    def update_audience_count(item):
+        if type(item['audience_count']) != list:
+            return
+
+        audience_count = [Config.deal_human_readable_numbers(i) for i in item['audience_count']]
+        item['audience_count'] = audience_count
+
+    @staticmethod
     def update_composed_pk(item):
         # audience_count = [Config.deal_human_readable_numbers(i) for i in item['audience_count']]
         item['platform_anchor'] = [item['platform'][0] + '+' + item['anchor'][0]]
 
     @staticmethod
+    def use_relative_url(item):
+        # audience_count = [Config.deal_human_readable_numbers(i) for i in item['audience_count']]
+        item['url'] = ['/' + item['url'][0].split('/')[-1]]
+
+    @staticmethod
     def preprocess_item(list_item):
         oi = OrderedDict(list_item)
 
-        # process_items_from_list(oi, Config.update_audience_count)
+        process_items_from_list(oi, Config.update_audience_count)
         process_items_from_list(oi, Config.update_composed_pk)
+        process_items_from_list(oi, Config.use_relative_url)
 
         return oi
